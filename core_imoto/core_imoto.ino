8083224e55f1dc28ebe6274d5591fd1c905ae634
Jasam <jreyro@gmail.com>
2015-02-06 00:57:32 -0500

    add functions

--- core_imoto/core_imoto.ino
+++ core_imoto/core_imoto.ino
@@ -1,6 +1,13 @@
 
 #include <SD.h>
 #include <EEPROM.h>
+#include <Time.h>  
+
+#define TIME_MSG_LEN  11   // time sync to PC is HEADER followed by unix time_t as ten ascii digits
+#define TIME_HEADER  'T'   // Header tag for serial time sync message
+#define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
+#define SYNC_INTERVAL 86400 //number of seconds between re-sync
+
 
 int RIGHT = 9;
 int LEFT = 8;
@@ -29,6 +36,14 @@ boolean stringComplete = false;  // whether the string is complete
 // name for arduino
 char sID[7];
 
+//Vars to handle time
+time_t timeNow;
+time_t timeNowBck;
+String strTimeNow;
+
+// flag for downloading
+boolean flagDownloading = false;
+
 void setup() {
   Serial.begin(9600); 
   pinMode(triggerRight, OUTPUT);
@@ -47,15 +62,20 @@ void setup() {
   }
   //Serial.println("initialization done.");
   inputString.reserve(50);
+  
+  setSyncInterval(SYNC_INTERVAL); //define sync interval
+  setSyncProvider( requestSync);  //set function to call when sync required
+  
 }
 
 void loop() {
   // Sensing
   rightDistance = senseUltraSonic(RIGHT);
   leftDistance = senseUltraSonic(LEFT);
-  
+  strTimeNow = strGetTime();
+  timeNowBck = getTime();
   // Saving sense data
-  saveData("IM0001",rightDistance, leftDistance, 1, millis());
+  saveData("IM0001",rightDistance, leftDistance, 1, strTimeNow);
   //Serial.println("Distancia derecha" + (String)rightDistance + " cm");
   //Serial.println("Distancia izquierda" + (String)leftDistance + " cm");
   
@@ -63,13 +83,22 @@ void loop() {
   if (stringComplete) {
     // Download data
     if (inputString == "d") {
+      flagDownloading = true;
       downLoadData();
+      flagDownloading = false;
       // clear the string:
+      inputString = "";
+      stringComplete = false;
+    } else {
+      Serial.println("el diablo!!!");
+      timeNow = inputString.toInt();
+      Serial.println(timeNow);
+      syncTime(timeNow);
       inputString = "";
       stringComplete = false;
     }
   }
-  
+
   delay(DELAY);
 }
 
@@ -96,14 +125,14 @@ int senseUltraSonic(int side) {
   return distance;
 }
 
-void saveData(String idComputer, int rightDistance, int leftDistance, int sPeed, int date){
+void saveData(String idComputer, int rightDistance, int leftDistance, int sPeed, String date){
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
   myFile = SD.open("imoto.csv", FILE_WRITE);
   
   // if the file opened okay, write to it:
   if (myFile) {
-    String registro = idComputer + "," + (String)rightDistance + "," + (String)leftDistance + "," + (String)sPeed + "," + (String)date;
+    String registro = idComputer + "," + (String)rightDistance + "," + (String)leftDistance + "," + (String)sPeed + "," + date;
     myFile.println(registro);
     // close the file:
     myFile.close();
@@ -156,4 +185,63 @@ void downLoadData() {
     Serial.println("error opening " + fileName);
   }
 }
-  
+
+void baptizeComputer(String name) {
+}
+
+//Service to sync Date and Time
+void syncTime(time_t pctime) {
+  Serial.println("En syncTime...");
+  setTime(pctime);   // Sync Arduino clock to the time received on the serial port
+}
+
+
+time_t requestSync()
+{
+  //Serial.write(TIME_REQUEST);  
+  return 0; // the time will be sent later in response to serial mesg
+}
+
+
+//Service to get date and time
+time_t getTime(){
+  time_t t = now();
+  return t;
+}
+
+//Service to get date and time as String
+String strGetTime(){
+  return (String)year()+"/"+(String)month()+"/"+(String)day()+" "+(String)hour()+":"+(String)minute()+":"+strPrintDigits(second());
+}
+
+String strPrintDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  if(digits < 10){
+    return '0'+(String)digits;
+  }  
+  return (String)digits;  
+}
+
+// digital clock display of the time
+void digitalClockDisplay(){
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year()); 
+  Serial.println(); 
+}  
+
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+
